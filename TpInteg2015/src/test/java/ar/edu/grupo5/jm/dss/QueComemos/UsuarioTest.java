6e86adc76227b703fd24b032890431ab332b862e
lean93 <leanrodri2093@gmail.com>
2015-04-24 16:04:21 -0300

    Agrego mi test... y añado el @before para crear los usuarios en el setUp

--- TpInteg2015/src/test/java/ar/edu/grupo5/jm/dss/QueComemos/UsuarioTest.java
+++ TpInteg2015/src/test/java/ar/edu/grupo5/jm/dss/QueComemos/UsuarioTest.java
@@ -1,16 +1,28 @@
 package ar.edu.grupo5.jm.dss.QueComemos;
 
 import static org.junit.Assert.assertEquals;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class UsuarioTest {
+	
+	private Usuario leandro;
+	private Usuario gustavo;
+	
+	@Before
+	public void setUp() {
+		gustavo = new Usuario(73, 1.83);
+		leandro = new Usuario(79, 1.78);
+	}
+	
 
 	@Test
 	public void gustavoTieneIMCDe2180(){
-		
-		Usuario gustavo = new Usuario(73, 1.83);
-		
 		assertEquals(21.80,gustavo.indiceMasaCorporal(),0.01);
+		
+	}
+	@Test 
+	public void leandroTieneIMCDe2499(){
+		assertEquals(24.93,leandro.indiceMasaCorporal(),0.01);
 	}
 }
