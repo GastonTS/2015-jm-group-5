f9d92a7177bc9d7658e91d104c28157f22d136ea
JuanchiRios <juanchirios04@hotmail.com>
2015-04-27 23:14:02 -0300

    estructura de user

---
+++ TpInteg2015/src/main/java/ar/edu/grupo5/jm/dss/QueComemos/CondicionPreexistente.java
@@ -0,0 +1,5 @@
+package ar.edu.grupo5.jm.dss.QueComemos;
+
+public class CondicionPreexistente {
+
+}
--- TpInteg2015/src/main/java/ar/edu/grupo5/jm/dss/QueComemos/Usuario.java
+++ TpInteg2015/src/main/java/ar/edu/grupo5/jm/dss/QueComemos/Usuario.java
@@ -1,17 +1,59 @@
 package ar.edu.grupo5.jm.dss.QueComemos;
 
+import java.time.LocalDate;
+import java.util.Collection;
+
+
 public class Usuario 
 {
 	private double peso;
 	private double estatura;
+	private String nombre;
+	private LocalDate fechaDeNacimiento;
+	private Collection<String> preferenciasAlimenticias;
+	private Collection<String> disgustosAlimenticios;
+	private Collection<Receta> recetasPublicas;
+	private Collection<Receta> recetasPropias;
+	private Collection<CondicionPreexistente> condicionesPreexistentes;
+	private Rutina rutina;
 	
-	public Usuario(double unPeso, double unaEstatura) {
+	
+	
+	public Usuario(double unPeso, double unaEstatura, String UnNombre,LocalDate UnaFechaDeNacimiento,
+			Collection<String> unasPreferenciasAlimenticias, Collection<String> unosDisgustosAlimenticios,
+			Collection<Receta> unasRecetasPublicas, Collection<Receta> unasRecetasPropias,
+			Collection<CondicionPreexistente> unasCondicionesPreexistentes, Rutina unaRutina) {
 		peso = unPeso;
 		estatura = unaEstatura;
+		fechaDeNacimiento = UnaFechaDeNacimiento;
+		preferenciasAlimenticias = unasPreferenciasAlimenticias;
+		disgustosAlimenticios = unosDisgustosAlimenticios;
+		recetasPublicas = unasRecetasPublicas;
+		recetasPropias = unasRecetasPropias;
+		condicionesPreexistentes = unasCondicionesPreexistentes;
+		rutina = unaRutina;
 	}
 	
 	public double indiceMasaCorporal() {
 		return peso / Math.pow(estatura,2);
 	}
 	
+	public boolean esUsuarioValido (){
+		//implementar!
+		return true;		
+	}
+	
+	public boolean sigueRutinaSaludable(){
+		//implementar!
+		return true;
+	
+	}
+	
+	public void crearReceta(){
+		//implementar es tipo Receta
+	}
+	
+	public void modificarReceta(){
+		//implementar es tipo Receta
+	}
 }
