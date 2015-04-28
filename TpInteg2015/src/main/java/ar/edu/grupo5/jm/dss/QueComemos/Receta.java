3b4ae7c3bd6db0de4d7cc8cdde246b5852c973e5
JuanchiRios <juanchirios04@hotmail.com>
2015-04-27 23:31:00 -0300

    primera estructura de receta

--- TpInteg2015/src/main/java/ar/edu/grupo5/jm/dss/QueComemos/Receta.java
+++ TpInteg2015/src/main/java/ar/edu/grupo5/jm/dss/QueComemos/Receta.java
@@ -1,5 +1,38 @@
 package ar.edu.grupo5.jm.dss.QueComemos;
 
-public class Receta {
+import java.util.Collection;
 
+public class Receta {
+	private String nombre;
+	private Collection<String> ingredientes;
+	private Collection<String> condimentos;
+	private Collection<Receta> subRecetas;
+	private String proceso;
+	private String temporada;
+	private String dificultad;
+	private double cantCalorias;
+	
+	
+	
+	public Receta(String unNombre,Collection<String> unosIngredientes, Collection<String> unosCondimentos,
+			Collection<Receta> unassubRecetas, String unProceso, String unaTemporada, String unaDificultad,
+			double unasCantCalorias) {
+		nombre = unNombre;
+		ingredientes = unosIngredientes;
+		condimentos = unosCondimentos;
+		subRecetas = unassubRecetas;
+		proceso = unProceso;
+		temporada = unaTemporada;
+		dificultad = unaDificultad;
+		cantCalorias = unasCantCalorias;
+	}
+	
+	//public Collection<CondicionPreexistente> esInadecuadaPara() {
+//		return;
+//	}
+	
+	public boolean esValida(){
+		//implementar
+		return true;
+	}
 }
