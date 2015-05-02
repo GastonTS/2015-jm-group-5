e24abac14b6a4192658c8908c6d78948655ab484
lean93 <leanrodri2093@gmail.com>
2015-05-02 10:07:44 -0300

    agrego las condiciones Preexistentes que faltan

---
+++ TpInteg2015/src/main/java/ar/edu/grupo5/jm/dss/QueComemos/Hipertenso.java
@@ -0,0 +1,20 @@
+package ar.edu.grupo5.jm.dss.QueComemos;
+
+public class Hipertenso implements CondicionPreexistente {
+
+	@Override
+	public boolean subsanaCondicion(Usuario unUsuario) {
+		return false;
+	}
+
+	@Override
+	public boolean esInadecuada(Receta unaReceta) {
+		return false;
+	}
+
+	@Override
+	public boolean esUsuarioValido(Usuario unUsuario) {
+		return unUsuario.tieneAlgunaPreferencia();
+	}
+
+}
