f21ddf0f8452b96420309ebca2bf8af75e2eed52
GastonTS <gaston-ts@hotmail.com>
2015-05-07 17:28:25 -0300

    Agrego los verify a CondimentosPreexistentesTest
    Y borro unos test, que me parecían con poca utilidad.

--- TpInteg2015/src/test/java/ar/edu/grupo5/jm/dss/QueComemos/CondicionesPreexistentesTest.java
+++ TpInteg2015/src/test/java/ar/edu/grupo5/jm/dss/QueComemos/CondicionesPreexistentesTest.java
@@ -39,46 +39,69 @@ public class CondicionesPreexistentesTest {
 	@Test
 	public void UsuarioSubsanaCondicionVegano() {
 		when(usuarioFalso.tienePreferencia("fruta")).thenReturn(true);
+		
 		assertTrue (vegano.subsanaCondicion(usuarioFalso));
+		
+		verify(usuarioFalso,times(1)).tienePreferencia("fruta");
 	}
 	
 	@Test
 	public void UsuarioNoSubsanaCondicionVegano() {
 		when(usuarioFalso.tienePreferencia("fruta")).thenReturn(false);
+		
 		assertFalse (vegano.subsanaCondicion(usuarioFalso));
+		
+		verify(usuarioFalso,times(1)).tienePreferencia("fruta");
 	}
 	
 
 	@Test
 	public void UsuarioSubsanaCondicionHipertenso() {
 		when(usuarioFalso.tieneRutinaIntensiva()).thenReturn(true);
+		
 		assertTrue (hipertenso.subsanaCondicion(usuarioFalso));
+		
+		verify(usuarioFalso,times(1)).tieneRutinaIntensiva();
 	}
 	
 	@Test
 	public void UsuarioNoSubsanaCondicionHipertenso() {
 		when(usuarioFalso.tieneRutinaIntensiva()).thenReturn(false);
+		
 		assertFalse (hipertenso.subsanaCondicion(usuarioFalso));
+		
+		verify(usuarioFalso,times(1)).tieneRutinaIntensiva();
 	}
 	
 	@Test
 	public void UsuarioSubsanaCondicionDiabeticoPorPeso() {
 		when(usuarioFalso.getPeso()).thenReturn(65.00);
+		
 		assertTrue (diabetico.subsanaCondicion(usuarioFalso));
+		
+		verify(usuarioFalso,times(1)).getPeso();
 	}
 	
 	@Test
 	public void UsuarioSubsanaCondicionDiabeticoPorRutina() {
 		when(usuarioFalso.getPeso()).thenReturn(85.00);
 		when(usuarioFalso.tieneRutinaActiva()).thenReturn(true);
+		
 		assertTrue (diabetico.subsanaCondicion(usuarioFalso));
+		
+		verify(usuarioFalso,times(1)).tieneRutinaActiva();
+		verify(usuarioFalso,times(1)).getPeso();
 	}
 	
 	@Test
 	public void UsuarioNoSubsanaCondicionDiabetic() {
 		when(usuarioFalso.getPeso()).thenReturn(85.00);
 		when(usuarioFalso.tieneRutinaActiva()).thenReturn(false);
+		
 		assertFalse (diabetico.subsanaCondicion(usuarioFalso));
+		
+		verify(usuarioFalso,times(1)).tieneRutinaActiva();
+		verify(usuarioFalso,times(1)).getPeso();
 	}
 	
 	//Tests de Usuarios validos para esa condicion
@@ -90,13 +113,19 @@ public class CondicionesPreexistentesTest {
 	@Test
 	public void UsuarioHipertensoValido() {
 		when(usuarioFalso.tieneAlgunaPreferencia()).thenReturn(true);
+		
 		assertTrue (hipertenso.esUsuarioValido(usuarioFalso));
+		
+		verify(usuarioFalso, times(1)).tieneAlgunaPreferencia();
 	}
 	
 	@Test
 	public void UsuarioHipertensoNoEsValido() {
 		when(usuarioFalso.tieneAlgunaPreferencia()).thenReturn(false);
+		
 		assertFalse (hipertenso.esUsuarioValido(usuarioFalso));
+		
+		verify(usuarioFalso, times(1)).tieneAlgunaPreferencia();
 	}
 	
 	
@@ -105,72 +134,73 @@ public class CondicionesPreexistentesTest {
 	public void UsuarioDiabeticoValido() {
 		when(usuarioFalso.indicaSexo()).thenReturn(true);
 		when(usuarioFalso.tieneAlgunaPreferencia()).thenReturn(true);
+		
 		assertTrue (diabetico.esUsuarioValido(usuarioFalso));
+		
+		verify(usuarioFalso, times(1)).tieneAlgunaPreferencia();
+		verify(usuarioFalso, times(1)).indicaSexo();
 	}
 	
 	@Test
 	public void UsuarioDiabeticoNoEsValido() {
 		when(usuarioFalso.indicaSexo()).thenReturn(false);
-		when(usuarioFalso.tieneAlgunaPreferencia()).thenReturn(true);
+		
 		assertFalse (diabetico.esUsuarioValido(usuarioFalso));
+		
+		verify(usuarioFalso, times(1)).indicaSexo();
 	}
 	
 	
 	@Test
 	public void UsuarioVeganoValido() {
 		when(usuarioFalso.tieneAlgunaDeEstasPreferencias(Vegano.getPreferenciasProhibidas())).thenReturn(false);
+		
 		assertTrue (vegano.esUsuarioValido(usuarioFalso));
+		
+		verify(usuarioFalso, times(1)).tieneAlgunaDeEstasPreferencias(Vegano.getPreferenciasProhibidas());
 	}
 	
 	@Test
 	public void UsuarioVeganoNoEsValido() {
 		when(usuarioFalso.tieneAlgunaDeEstasPreferencias(Vegano.getPreferenciasProhibidas())).thenReturn(true);
+		
 		assertFalse (vegano.esUsuarioValido(usuarioFalso));
+		
+
+		verify(usuarioFalso, times(1)).tieneAlgunaDeEstasPreferencias(Vegano.getPreferenciasProhibidas());
 	}
 	
+	//Tests de receta inadecuada para esa condicion
 	@Test
-	public void recetaCeliacaInvalida() {
+	public void recetaCeliacaSiempreEsAdecuada() {
 	
 		assertFalse (celiaco.esInadecuada(recetaFalsa));
 	}
 	
 	@Test
-	public void recetaVeganaInvalida() {
+	public void recetaVeganaEsInadecuadaSiTieneIngredientesProhibidos() {
 		when(recetaFalsa.tenesAlgunIngredienteDeEstos(Vegano.getPreferenciasProhibidas())).thenReturn(true);
+		
 		assertTrue (vegano.esInadecuada(recetaFalsa));
+
+		verify(recetaFalsa, times(1)).tenesAlgunIngredienteDeEstos(Vegano.getPreferenciasProhibidas());
 	}
 	
 	@Test
-	public void recetaDiabeticaInvalida() {
+	public void recetaDiabeticaEsInadecuadaSiTieneCondimentosProhibidos() {
 		when(recetaFalsa.tenesMasDe(Diabetico.GetCondimentosProhibidos())).thenReturn(true);
+		
 		assertTrue (diabetico.esInadecuada(recetaFalsa));
+		
+		verify(recetaFalsa, times(1)).tenesMasDe(Diabetico.GetCondimentosProhibidos());
 	}
 	
 	@Test
-	public void recetaHipertensaInvalida() {
+	public void recetaHipertensaEsInadecuadaSiTieneCondimentosProhibidos() {
 		when(recetaFalsa.tenesAlgoDe(Hipertenso.getCondimentosProhibidos())).thenReturn(true);
+		
 		assertTrue (hipertenso.esInadecuada(recetaFalsa));
+		
+		verify(recetaFalsa, times(1)).tenesAlgoDe(Hipertenso.getCondimentosProhibidos());
 	}
-	
-	//Tests de receta inadecuada para esa condicion
-	@Test
-	public void RecetaValidaHipertenso(){
-		when(recetaFalsa.tenesAlgoDe(Hipertenso.getCondimentosProhibidos())).thenReturn(false);
-		assert (hipertenso.esInadecuada(recetaFalsa));
-	}
-	
-	@Test
-	public void RecetaValidaDiabetico(){
-		when(recetaFalsa.tenesMasDe(Diabetico.GetCondimentosProhibidos())).thenReturn(false);
-		assert (diabetico.esInadecuada(recetaFalsa));
-	}
-	
-	@Test
-	public void RecetaValidaVegano(){
-		when(recetaFalsa.tenesAlgunIngredienteDeEstos(Vegano.getPreferenciasProhibidas())).thenReturn(false);
-		assert (vegano.esInadecuada(recetaFalsa));
-	}
-	
-	
-	
 }
