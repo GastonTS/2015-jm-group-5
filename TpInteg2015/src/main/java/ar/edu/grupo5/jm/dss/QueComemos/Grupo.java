fcf44466b1c7a2b1dc05a5e80e76802bfcd166b2
Ramiro Vazquez <ramirov93@gmail.com>
2015-05-22 21:03:41 -0300

    Agrego la clase Grupo que por ahora no tiene implementado métodos

---
+++ TpInteg2015/src/main/java/ar/edu/grupo5/jm/dss/QueComemos/Grupo.java
@@ -0,0 +1,22 @@
+package ar.edu.grupo5.jm.dss.QueComemos;
+
+import java.util.Collection;
+
+public class Grupo {
+
+	private String nombre;
+	private Collection<String> preferenciasAlimenticias;
+	private Collection<Usuario> integrantes;
+
+	public Grupo(String unNombre,
+			Collection<String> unasPreferenciasAlimenticias,
+			Collection<Usuario> unosIntegrantes) {
+		nombre = unNombre;
+		preferenciasAlimenticias = unasPreferenciasAlimenticias;
+		integrantes = unosIntegrantes;
+	}
+
+	public boolean puedeSugerirse(Receta unaReceta){
+		return true;
+	}
+}
