122c8eaf52e1e5499ecfb709bdf989480de8a301
GastonTS <gaston-ts@hotmail.com>
2015-05-23 15:59:27 -0300

    Falto IFiltro - SinFiltro

---
+++ TpInteg2015/src/main/java/ar/edu/grupo5/jm/dss/QueComemos/IFiltro.java
@@ -0,0 +1,9 @@
+package ar.edu.grupo5.jm.dss.QueComemos;
+
+import java.util.Collection;
+
+public interface IFiltro {
+
+	public abstract Collection<Receta> filtrarRecetas(Collection<Receta> recetas);
+
+}
---
+++ TpInteg2015/src/main/java/ar/edu/grupo5/jm/dss/QueComemos/SinFiltro.java
@@ -0,0 +1,12 @@
+package ar.edu.grupo5.jm.dss.QueComemos;
+
+import java.util.Collection;
+
+public class SinFiltro implements IFiltro{
+	
+	@Override
+	public Collection<Receta> filtrarRecetas(Collection<Receta> recetas){
+		return recetas;
+	}
+
+}
