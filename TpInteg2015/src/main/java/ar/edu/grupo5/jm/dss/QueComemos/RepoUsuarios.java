d2bb192c8eb49e592f4d4503afdee7f73324fd8b
JuanchiRios <juanchirios04@hotmail.com>
2015-06-03 17:27:38 -0300

    empiezo hacer la clase RepoUsuarios para el punto 1 de la entrega 3

---
+++ TpInteg2015/src/main/java/ar/edu/grupo5/jm/dss/QueComemos/RepoUsuarios.java
@@ -0,0 +1,23 @@
+package ar.edu.grupo5.jm.dss.QueComemos;
+
+import java.util.Collection;
+
+public class RepoUsuarios {
+	private Collection<Usuario> usuarios;
+
+	public void add(Usuario unUsuario) {
+		usuarios.add(unUsuario);
+	}
+
+	public void remove(Usuario unUsuario) {
+		usuarios.remove(unUsuario);
+	}
+	/*
+update(Usuario)
+get(Usuario): devuelve un usuario haciendo la búsqueda por el nombre del usuario que se pase como parámetro
+list(Usuario): devuelve una lista de usuarios cuyo nombre contenga el nombre del usuario ingresado como parámetro. 
+En caso de ingresar condiciones preexistentes, devuelve todos los usuarios que cumplen con la condición del nombre
+ anteriormente descrita y que además cumplan todas las condiciones preexistentes, ej: todos los diabéticos.
+	 * 
+	 */
+}
