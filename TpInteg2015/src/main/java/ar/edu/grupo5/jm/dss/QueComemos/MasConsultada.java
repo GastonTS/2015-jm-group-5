dc9ed25f05169a658e114d62dab3cd5bf3a1620f
Ramiro Vazquez <ramirov93@gmail.com>
2015-06-07 13:42:08 -0300

    MasConsultada modificada y testeada (pero falta hacer un método que de la misma que falla)

--- TpInteg2015/src/main/java/ar/edu/grupo5/jm/dss/QueComemos/MasConsultada.java
+++ TpInteg2015/src/main/java/ar/edu/grupo5/jm/dss/QueComemos/MasConsultada.java
@@ -1,23 +1,37 @@
 package ar.edu.grupo5.jm.dss.QueComemos;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Optional;
 
 public class MasConsultada implements ObservadorConsultas {
-	Map<Receta, Integer> recetaMap = new HashMap<Receta, Integer>();
+	Collection<String> nombreRecetasConsultadas = new ArrayList<String>();
 
 	@Override
 	public void notificar(Usuario unUsuario,
 			Collection<Receta> recetasConsultadas) {
-		recetasConsultadas.forEach(unaReceta -> agregarUnaConsultaAlMapReceta(unaReceta));
+		recetasConsultadas.forEach(unaReceta -> agregarUnaConsulta(unaReceta));
 	}
 
-	//VER: Qué utilizo para identificar a una Receta?? Su nombre, todo el objeto en sí, otras?
-	private void agregarUnaConsultaAlMapReceta(Receta unaReceta){
-		recetaMap.put(unaReceta, recetaMap.get(unaReceta) + 1);
+	// VER: Qué utilizo para identificar a una Receta?? Su nombre, todo el
+	// objeto en sí, otras?
+	private void agregarUnaConsulta(Receta unaReceta) {
+		nombreRecetasConsultadas.add(unaReceta.getNombre());
+	}
+
+	// debería agregar un getter de recetaMap para saber que
+	// funciona correctamente, lo que luego se probaría en un test
+	public Optional<String> nombreRecetaMasConsultada() {
+		return nombreRecetasConsultadas.stream().max((unNombre, otroNombre) -> cantidadDeConsultas(unNombre) - cantidadDeConsultas(otroNombre));
 	}
 	
-	// debería agregar un getter de recetaMap para saber que
-	// funciona correctamente, lo que luego se probaría en un test	
+	private int cantidadDeConsultas(String nombreDeReceta){
+		return Collections.frequency(nombreRecetasConsultadas, nombreDeReceta);
+	}
+	
+	public int cantidadDeConsultasDeRecetaMAsConsultada() {
+		return cantidadDeConsultas(nombreRecetaMasConsultada().toString());
+	}
 }
--- TpInteg2015/src/test/java/ar/edu/grupo5/jm/dss/QueComemos/ObserverTest.java
+++ TpInteg2015/src/test/java/ar/edu/grupo5/jm/dss/QueComemos/ObserverTest.java
@@ -9,6 +9,7 @@ import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
+import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -23,8 +24,13 @@ public class ObserverTest {
 	private Receta vegetarianaMock = mock(Receta.class);
 
 	private Collection<Receta> recetas = new ArrayList<Receta>();
+	private Collection<Receta> recetasDeGuisoYPancho = new ArrayList<Receta>();
+	private Collection<Receta> recetasDePanchoYEnsalada = new ArrayList<Receta>();
 
 	PorHoraDelDia observerPorHoraDelDia = new PorHoraDelDia();
+	int horaActual, horaSiguiente;
+	MasConsultada observerRecetaMasConsultada = new MasConsultada();
+
 	SegunSexo observerSegunSexo = new SegunSexo();
 
 	@Before
@@ -33,6 +39,16 @@ public class ObserverTest {
 		recetas.add(ensaladaMock);
 		recetas.add(panchoMock);
 		recetas.add(vegetarianaMock);
+		recetasDeGuisoYPancho.add(guisoMock);
+		recetasDeGuisoYPancho.add(panchoMock);
+		recetasDePanchoYEnsalada.add(panchoMock);
+		recetasDePanchoYEnsalada.add(ensaladaMock);
+
+		horaActual = Calendar.HOUR_OF_DAY;
+		if (horaActual == 23)
+			horaSiguiente = 0;
+		else
+			horaSiguiente = horaActual + 1;
 	}
 
 	@Test
@@ -40,21 +56,38 @@ public class ObserverTest {
 		observerPorHoraDelDia.notificar(usuarioMock, recetas);
 		observerPorHoraDelDia.notificar(usuarioMock, recetas);
 		assertEquals(
-				observerPorHoraDelDia
-						.getConsultasPorHoraDelDia(Calendar.HOUR_OF_DAY),
-				2);
+				observerPorHoraDelDia.getConsultasPorHoraDelDia(horaActual), 2);
 		assertEquals(
-				observerPorHoraDelDia
-						.getConsultasPorHoraDelDia(Calendar.HOUR_OF_DAY + 1),
+				observerPorHoraDelDia.getConsultasPorHoraDelDia(horaSiguiente),
 				0);
 	}
-	
+
+	@Test
+	public void devuelveNombreYCantidadDeConsultasDeRecetaMasConsultada() {
+		when(guisoMock.getNombre()).thenReturn("guiso");
+		when(ensaladaMock.getNombre()).thenReturn("ensalada");
+		when(panchoMock.getNombre()).thenReturn("pancho");
+		when(vegetarianaMock.getNombre()).thenReturn("vegetariana");
+		
+		observerRecetaMasConsultada.notificar(usuarioMock, recetas);
+		observerRecetaMasConsultada.notificar(usuarioMock, recetasDeGuisoYPancho);
+		observerRecetaMasConsultada.notificar(usuarioMock, recetasDePanchoYEnsalada);
+		
+		assertEquals(observerRecetaMasConsultada.nombreRecetaMasConsultada(), Optional.of("pancho"));
+		//assertEquals(observerRecetaMasConsultada.cantidadDeConsultasDeRecetaMAsConsultada(), 3);
+		
+		verify(guisoMock, times(2)).getNombre();
+		verify(ensaladaMock, times(2)).getNombre();
+		verify(panchoMock, times(3)).getNombre();
+		verify(vegetarianaMock, times(1)).getNombre();
+	}
+
 	@Test
 	public void usuarioMasculinoConsulta4Recetas() {
 		when(usuarioMock.esDeSexo("Masculino")).thenReturn(true);
 		observerSegunSexo.notificar(usuarioMock, recetas);
-		assertEquals(observerSegunSexo.getConsultasMasculinas(),4);
-		assertEquals(observerSegunSexo.getConsultasFemeninas(),0);
+		assertEquals(observerSegunSexo.getConsultasMasculinas(), 4);
+		assertEquals(observerSegunSexo.getConsultasFemeninas(), 0);
 		verify(usuarioMock, times(1)).esDeSexo("Masculino");
 	}
 }
