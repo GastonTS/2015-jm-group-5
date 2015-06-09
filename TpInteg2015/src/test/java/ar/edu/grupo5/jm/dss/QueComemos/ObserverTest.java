d52f3d771fbf3267f0a8c2928eb49f8cf7b295d3
GastonTS <gaston-ts@hotmail.com>
2015-06-09 13:47:57 -0300

    Test Consultas Dificiles Veganos

--- TpInteg2015/src/test/java/ar/edu/grupo5/jm/dss/QueComemos/ObserverTest.java
+++ TpInteg2015/src/test/java/ar/edu/grupo5/jm/dss/QueComemos/ObserverTest.java
@@ -15,18 +15,22 @@ import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
 
+import ar.edu.grupo5.jm.dss.QueComemos.Oberserver.ConsultaVeganoRecetasDificles;
 import ar.edu.grupo5.jm.dss.QueComemos.Oberserver.MasConsultada;
 import ar.edu.grupo5.jm.dss.QueComemos.Oberserver.PorHoraDelDia;
 import ar.edu.grupo5.jm.dss.QueComemos.Oberserver.SegunSexo;
 import ar.edu.grupo5.jm.dss.QueComemos.Receta.Receta;
+import ar.edu.grupo5.jm.dss.QueComemos.Usuario.CondicionDeSalud;
 import ar.edu.grupo5.jm.dss.QueComemos.Usuario.Usuario;
+import ar.edu.grupo5.jm.dss.QueComemos.Usuario.Vegano;
 
 public class ObserverTest {
 
 	private Usuario usuarioMock = mock(Usuario.class);
-	private Usuario usuraioMockFem = mock(Usuario.class);
-	private Usuario veganoMock = mock(Usuario.class);
-
+	private Usuario usuarioMockFem = mock(Usuario.class);
+	private Usuario vegano;
+	private Usuario otroVegano;
+	
 	private Receta guisoMock = mock(Receta.class);
 	private Receta ensaladaMock = mock(Receta.class);
 	private Receta panchoMock = mock(Receta.class);
@@ -37,9 +41,13 @@ public class ObserverTest {
 	private Collection<Receta> recetasDePanchoYEnsalada = new ArrayList<Receta>();
 	private Collection<Receta> recetaExtraEnsalada = new ArrayList<Receta>();
 
+	private Vegano condicionMock = mock(Vegano.class);
+	private Collection<CondicionDeSalud> condicionesDeSalud = Arrays.asList(condicionMock);
+	
 	PorHoraDelDia observerPorHoraDelDia = new PorHoraDelDia();
 	MasConsultada observerRecetaMasConsultada = new MasConsultada();
 	SegunSexo observerSegunSexo = new SegunSexo();
+	ConsultaVeganoRecetasDificles observerConsultaVeganoRecetasDificiles = new ConsultaVeganoRecetasDificles();
 
 	@Before
 	public void setUp() {
@@ -47,6 +55,9 @@ public class ObserverTest {
 		recetasDeGuisoYPancho = Arrays.asList(guisoMock, panchoMock);
 		recetasDePanchoYEnsalada = Arrays.asList(panchoMock, ensaladaMock);
 		recetaExtraEnsalada = Arrays.asList(ensaladaMock, ensaladaMock);
+		
+		vegano = new Usuario(null, null, new ArrayList<String>(), new ArrayList<String>(), condicionesDeSalud, null);
+		otroVegano = new Usuario(null, null, new ArrayList<String>(), new ArrayList<String>(), condicionesDeSalud, null);
 	}
 
 	@Test
@@ -64,10 +75,9 @@ public class ObserverTest {
 		assertEquals(observerPorHoraDelDia.getConsultasPorHoraDelDia(horaSiguiente), 0);
 	}
 
-
 	@Test
 	public void devuelveNombreYCantidadDeConsultasDeRecetaMasConsultada() {
-		
+
 		observerRecetaMasConsultada.notificar(usuarioMock, recetas);
 		observerRecetaMasConsultada.notificar(usuarioMock, recetasDeGuisoYPancho);
 		observerRecetaMasConsultada.notificar(usuarioMock, recetasDePanchoYEnsalada);
@@ -81,15 +91,15 @@ public class ObserverTest {
 	public void cantidadYNombreDeRecetasConsultadasDeHombresYMujeres() {
 
 		when(usuarioMock.esDeSexo("Masculino")).thenReturn(true);
-		when(usuraioMockFem.esDeSexo("Femenino")).thenReturn(true);
+		when(usuarioMockFem.esDeSexo("Femenino")).thenReturn(true);
 
 		observerSegunSexo.notificar(usuarioMock, recetas);
 		observerSegunSexo.notificar(usuarioMock, recetasDeGuisoYPancho);
 		observerSegunSexo.notificar(usuarioMock, recetasDePanchoYEnsalada);
 
-		observerSegunSexo.notificar(usuraioMockFem, recetas);
-		observerSegunSexo.notificar(usuraioMockFem, recetasDePanchoYEnsalada);
-		observerSegunSexo.notificar(usuraioMockFem, recetaExtraEnsalada);
+		observerSegunSexo.notificar(usuarioMockFem, recetas);
+		observerSegunSexo.notificar(usuarioMockFem, recetasDePanchoYEnsalada);
+		observerSegunSexo.notificar(usuarioMockFem, recetaExtraEnsalada);
 
 		assertEquals(observerSegunSexo.recetaHombre(), Optional.of(panchoMock));
 		assertEquals(observerSegunSexo.cantidadRecetaMasConsultadaHombre(), 3);
@@ -98,8 +108,23 @@ public class ObserverTest {
 		assertEquals(observerSegunSexo.cantidadRecetaMasConsultadaMujer(), 4);
 
 		verify(usuarioMock, times(3)).esDeSexo("Masculino");
-		verify(usuraioMockFem, times(3)).esDeSexo("Femenino");
+		verify(usuarioMockFem, times(3)).esDeSexo("Femenino");
 	}
 
-}
+	@Test
+	public void cantidadDeVeganosQueConsultanRecetasDificiles() {
+		when(guisoMock.esDificil()).thenReturn(true);
+		when(condicionMock.deboNotificar()).thenReturn(true);
 
+		observerConsultaVeganoRecetasDificiles.notificar(vegano, recetas);
+		observerConsultaVeganoRecetasDificiles.notificar(otroVegano, recetasDeGuisoYPancho);
+		observerConsultaVeganoRecetasDificiles.notificar(usuarioMock, recetasDePanchoYEnsalada);
+
+		assertEquals(observerConsultaVeganoRecetasDificiles.cantidadDeVeganos(), 2);
+
+		verify(guisoMock, times(2)).esDificil();
+		verify(panchoMock, times(1)).esDificil();
+		verify(ensaladaMock, times(1)).esDificil();
+		verify(condicionMock, times(2)).deboNotificar();
+	}
+}
