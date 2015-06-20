d3d2348316188f2d546b2359d2bb11e4b0eb5044
GastonTS <gaston-ts@hotmail.com>
2015-06-20 01:10:35 -0300

    Fix tests (nullpointer,)

--- TpInteg2015/src/main/java/ar/edu/grupo5/jm/dss/QueComemos/MonitoreoAsincronico/BufferDeConsultas.java
+++ TpInteg2015/src/main/java/ar/edu/grupo5/jm/dss/QueComemos/MonitoreoAsincronico/BufferDeConsultas.java
@@ -1,12 +1,13 @@
 package ar.edu.grupo5.jm.dss.QueComemos.MonitoreoAsincronico;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class BufferDeConsultas {
 
 	public static BufferDeConsultas instancia = new BufferDeConsultas();
-	private Collection<Consulta> consultasRealizadas;
-	private Collection<ProcesoAsincronico> procesosARealizar;
+	private Collection<Consulta> consultasRealizadas = new ArrayList<Consulta>();
+	private Collection<ProcesoAsincronico> procesosARealizar = new ArrayList<ProcesoAsincronico>();
 
 	public void agregarConsulta(Consulta unaConsulta) {
 		consultasRealizadas.add(unaConsulta);
