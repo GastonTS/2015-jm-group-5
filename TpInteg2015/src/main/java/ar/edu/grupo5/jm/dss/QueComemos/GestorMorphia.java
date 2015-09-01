0a08a60d1416dece35b1a7ac37192c6611dbb1c6
GastonTS <gastonsantoalla@gmail.com>
2015-09-01 18:37:08 -0300

    Gestor Morphia (Si, no me convence el nombre tampoco, no se me ocurre otro) Tambien hay que revisar si vale la pena que sea una clase separada o no

---
+++ TpInteg2015/src/main/java/ar/edu/grupo5/jm/dss/QueComemos/GestorMorphia.java
@@ -0,0 +1,20 @@
+package ar.edu.grupo5.jm.dss.QueComemos;
+
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
+
+import com.mongodb.MongoClient;
+
+public class GestorMorphia {
+	
+	public static GestorMorphia instancia = new GestorMorphia();
+	private Datastore datastore;
+	Morphia morphia = new Morphia();
+
+	public GestorMorphia() {
+		morphia.mapPackage("Usuario");
+		morphia.mapPackage("Receta");
+		datastore = morphia.createDatastore(new MongoClient(), "recetasTest");
+	}
+
+}
