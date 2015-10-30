ff0d99c2536f84e38163d72374f1adc1c9f9acb0
lean93 <leanrodri2093@gmail.com>
2015-10-30 01:33:47 -0300

    mostrar listado de usuarios...

--- src/main/java/ar/edu/grupo5/jm/dss/QueComemos/Controllers/UsuariosController.java
+++ src/main/java/ar/edu/grupo5/jm/dss/QueComemos/Controllers/UsuariosController.java
@@ -1,14 +1,39 @@
 package ar.edu.grupo5.jm.dss.QueComemos.Controllers;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
+import ar.edu.grupo5.jm.dss.QueComemos.Receta.Ingrediente;
+import ar.edu.grupo5.jm.dss.QueComemos.Usuario.CondicionDeSalud.CondicionDeSalud;
+import ar.edu.grupo5.jm.dss.QueComemos.Usuario.DatosPersonales.Sexo;
+import ar.edu.grupo5.jm.dss.QueComemos.Usuario.Usuario;
+import ar.edu.grupo5.jm.dss.QueComemos.Usuario.Complexion;
+import ar.edu.grupo5.jm.dss.QueComemos.Usuario.Usuario.Rutina;
+import ar.edu.grupo5.jm.dss.QueComemos.Usuario.DatosPersonales;
 
 
 public class UsuariosController {
-
+	
 	 public ModelAndView listar(Request request, Response response) {
+		 Collection<Usuario> listaUsuario = new ArrayList<Usuario>();
+		 
+		  Usuario lean = new Usuario((new DatosPersonales("leandro",Sexo.MASCULINO,LocalDate.parse("1993-11-09")))
+					,new Complexion(70,150)
+			, new ArrayList<Ingrediente>()
+			,new ArrayList<Ingrediente>()
+			, new ArrayList<CondicionDeSalud>()
+			,Rutina.MEDIANA, "lean.com");
 		   
-		    return new ModelAndView(null, "usuarios.hbs");
+		  listaUsuario.add(lean);
+			
+			 HashMap<String, Object> viewModel = new HashMap<>();
+			    viewModel.put("usuarios", listaUsuario);
+			   	
+		    return new ModelAndView(viewModel, "usuarios.hbs");
 	}
 }
