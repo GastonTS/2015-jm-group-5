883e81e57b51c87f0cab18cb9f3d297c940626b1
Ramiro Vazquez <ramirov93@gmail.com>
2015-11-02 09:40:27 -0300

    Saco cosas que estaban de más

--- src/main/java/ar/edu/grupo5/jm/dss/QueComemos/Main/Bootstrap.java
+++ src/main/java/ar/edu/grupo5/jm/dss/QueComemos/Main/Bootstrap.java
@@ -1,17 +1,8 @@
 package ar.edu.grupo5.jm.dss.QueComemos.Main;
 
-import java.util.Arrays;
-
 import org.uqbarproject.jpa.java8.extras.EntityManagerOps;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
-
-import ar.edu.grupo5.jm.dss.QueComemos.Receta.Ingrediente;
-import ar.edu.grupo5.jm.dss.QueComemos.Receta.RecetaBuilder;
-import ar.edu.grupo5.jm.dss.QueComemos.Receta.Recetario;
-import ar.edu.grupo5.jm.dss.QueComemos.Receta.Receta.Dificultad;
-import ar.edu.grupo5.jm.dss.QueComemos.Usuario.CondicionDeSalud.CondicionDeSalud;
-import ar.edu.grupo5.jm.dss.QueComemos.Usuario.CondicionDeSalud.Vegano;
 
 
 public class Bootstrap implements WithGlobalEntityManager, EntityManagerOps, TransactionalOps {
@@ -21,18 +12,6 @@ public class Bootstrap implements WithGlobalEntityManager, EntityManagerOps, Tra
   }
 
   public void run() {
-    	
-    	Vegano asd = new Vegano();
-    	
-    	persist(asd);
-    	
-    	
-//    	Ingrediente pimienta = new Ingrediente("pimienta");
-//	    
-//	    entityManager().persist(pimienta);
-//    
-//    	Recetario.instancia.setRecetasTotales(Arrays.asList(new RecetaBuilder().setNombre("Receta 1")
-//	    		.agregarIngrediente(pimienta).setCantCalorias(1000).setDificultad(Dificultad.ALTA).construirReceta()));
   }
 
 }
