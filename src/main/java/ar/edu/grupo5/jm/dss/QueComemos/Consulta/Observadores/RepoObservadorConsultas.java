ccb5ecd08c4032cb6b5a1d5d717655244685dd0d
gcrespi <crespi.gustavo@gmail.com>
2015-11-05 02:53:43 -0300

    Creo repoObservadores

---
+++ src/main/java/ar/edu/grupo5/jm/dss/QueComemos/Consulta/Observadores/RepoObservadorConsultas.java
@@ -0,0 +1,39 @@
+package ar.edu.grupo5.jm.dss.QueComemos.Consulta.Observadores;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
+
+import ar.edu.grupo5.jm.dss.QueComemos.Receta.Receta;
+import ar.edu.grupo5.jm.dss.QueComemos.Usuario.Usuario;
+
+public class RepoObservadorConsultas implements WithGlobalEntityManager {
+
+	public static RepoObservadorConsultas instancia = new RepoObservadorConsultas();
+
+	public void quitarObservador(ObservadorConsultas unObservador) {
+		entityManager().remove(unObservador);
+	}
+
+	public void agregarObservador(ObservadorConsultas unObservador) {
+		entityManager().persist(unObservador);
+	}
+
+	public Collection<ObservadorConsultas> getObservadores() {
+		return entityManager().createQuery("FROM ObservadorConsultas", ObservadorConsultas.class).getResultList();
+	}
+
+	public void notificarObservadores(Usuario unUsuario, Collection<Receta> recetasConsultadas) {
+		this.getObservadores().forEach(observador -> observador.notificarConsulta(unUsuario, recetasConsultadas));
+	}
+
+	public <TipoObservador> Optional<TipoObservador> getObservador(String discriminante, Class<TipoObservador> tipo) {
+		List<TipoObservador> observadores = entityManager().createQuery("FROM ObservadorConsultas WHERE DTYPE = :discriminante", tipo)
+				.setParameter("discriminante", discriminante)
+				.getResultList();
+		
+		return (observadores.isEmpty())?Optional.empty() : Optional.of(observadores.get(0));
+	}
+}
